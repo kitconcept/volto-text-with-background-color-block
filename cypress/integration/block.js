40ba0f72f30320fd5c78e6e67a620128e72517a9
Rob Gietema <rob.gietema@gmail.com>
2022-04-19 20:31:49 +0200

    Added github workflow.

--- cypress/integration/block.js
+++ cypress/integration/block.js
@@ -13,9 +13,10 @@ context('Block Acceptance Tests', () => {
 
   it('As editor I can add a Text With Background Color block', () => {
     cy.visit('/document/edit');
-    cy.get('.block .slate-editor [contenteditable=true]').click();
+    cy.get('.block.inner.text .public-DraftEditor-content').click();
     cy.get('.button .block-add-button').click({ force: true });
-    cy.get('.button.textWithBackgroundColor').click();
+    cy.findByLabelText('Text').click();
+    cy.get('.blocks-chooser .mostUsed .button.textWithBackgroundColor').click();
     cy.get('#toolbar-save').click();
   });
 }
